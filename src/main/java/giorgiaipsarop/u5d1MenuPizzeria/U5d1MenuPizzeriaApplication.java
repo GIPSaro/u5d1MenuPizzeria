17f1fb2e8f491853a1e8d154fb683aee2f7a3bc0
Giorgia Ipsaro Passione <145654021+GIPSaro@users.noreply.github.com>
2024-09-02 15:19:30 +0200

    Initial commit

---
+++ src/main/java/giorgiaipsarop/u5d1MenuPizzeria/U5d1MenuPizzeriaApplication.java
@@ -0,0 +1,13 @@
+package giorgiaipsarop.u5d1MenuPizzeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5d1MenuPizzeriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5d1MenuPizzeriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/giorgiaipsarop/u5d1MenuPizzeria/U5d1MenuPizzeriaApplicationTests.java
@@ -0,0 +1,13 @@
+package giorgiaipsarop.u5d1MenuPizzeria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5d1MenuPizzeriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
